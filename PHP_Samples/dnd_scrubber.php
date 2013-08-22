42d959590266ecff0f7155cd6579879f5875abb0
Shivku <shivku@exotel.in>
2013-08-22 19:36:13 +0530

    Bug fix. Wrong comparison in If construct

--- PHP_Samples/dnd_scrubber.php
+++ PHP_Samples/dnd_scrubber.php
@@ -13,7 +13,8 @@ $exotel_sid = "xxxx"; // Your Exotel SID - Get it from here: http://my.exotel.in
 $exotel_token = "xxxx"; // Your exotel token - Get it from here: http://my.exotel.in/Exotel/settings/site#api-settings
 
 foreach($numbers as $number) {
- 
+
+	$number = trim($number); 
 	$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Numbers/$number";
 
 	echo "Checking if $number is DND or not...\n";
@@ -34,7 +35,7 @@ foreach($numbers as $number) {
 	if($error) continue; 
 
 	$xml = simplexml_load_string($http_result); 
-	if($xml->Numbers->DND == "Yes") {
+	if($xml->Numbers->DND != "Yes") {
 		$dnd_free_numbers[] = $number;
 	}
 
